baa6fd0f6115cdae667c13cb870b5d24a8a5a642
Joaquin <joaquin9918@gmail.com>
2023-02-02 19:43:57 -0300

    desafio 2 fixed

--- clase7/src/index.js
+++ clase7/src/index.js
@@ -1,15 +1,3 @@
-/* import http from 'http'
-
-const PORT = 4000
-
-const server = http.createServer((req, res)=>{
-    res.end(`Hola este es mi primer servidor`)
-})
-
-server.listen(PORT, ()=>{
-console.log(`Server run on port ${PORT}`);
-}) */
-
 import express from "express";
 
 import ProductManager from "../../desafio-2/ProductManager.js";
@@ -18,32 +6,33 @@ const manager = new ProductManager("../../Backend 39685/desafio-2/data.json");
 
 const app = express(); // app es igual a la ejecucion de express
 
-app.use(express.urlencoded({extended: true})) // Permite realizar consultas den la URL (req.query)
+app.use(express.urlencoded({ extended: true })); // Permite realizar consultas den la URL (req.query)
 
-const PORT = 4000;
+const PORT = 8080;
 
 //Ruta raiz
 app.get("/", (req, res) => {
   res.send("Primer servidor con express");
 });
 
+app.get("/products", async (req, res) => {
+  const products = await manager.getProducts();
+  let { limit } = req.query;
+  if (limit) {
+    res.send(products.slice(0, limit));
+  } else {
+    res.send(products);
+  }
+});
 
-app.get("/products", async (req, res)=>{
-const products = await manager.getProducts()
-let {limit} = req.query
-if(limit){
-    res.send(products.slice(0, limit))
-}else{
-    res.send(products)
-}
-
-})
-
-app.get("/products/:id", async (req, res)=>{
-const product = await manager.getById(parseInt(req.params.id))
-console.log(product);
-res.send(product)
-})
+app.get("/products/:id", async (req, res) => {
+  const product = await manager.getById(parseInt(req.params.id));
+  if (product) {
+    res.send(product);
+  } else {
+    res.json({ Error: "id not found" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
