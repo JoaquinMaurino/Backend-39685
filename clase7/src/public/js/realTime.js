309252c1a051a7563d66a2981f3a96125c2eab1e
Joaquin <joaquin9918@gmail.com>
2023-02-23 21:42:39 -0300

    Desafio socket.io correcciones

--- clase7/src/public/js/realTime.js
+++ clase7/src/public/js/realTime.js
@@ -1,7 +1,11 @@
 const socket = io.connect();
 const form = document.getElementById("formId");
 const productTitle = document.getElementById("title");
+const productDesc = document.getElementById("desc");
 const productPrice = document.getElementById("price");
+const productCode = document.getElementById("code");
+const productStock = document.getElementById("stock");
+const productCat = document.getElementById("cat");
 const btnDelete = document.getElementById("deleteAll");
 
 function renderData(data) {
@@ -10,9 +14,15 @@ function renderData(data) {
     document.getElementById("products").innerHTML += `
       <tr>
         <th scope="row"></th>
-        <td>${product.title}</td>
         <td>${product.id}</td>
-        <td>$${product.price}</td>
+        <td>${product.title}</td>
+        <td>${product.description}</td>
+        <td>${product.img}</td>
+        <td>${product.price}</td>
+        <td>${product.code}</td>
+        <td>${product.stock}</td>
+        <td>${product.category}</td>
+        <td>${product.status}</td>
         <td scope="col"><Button class="btn btn-danger" onclick="deleteProduct(${product.id})">Eliminar</Button></th>
       </tr>
     `;
@@ -32,12 +42,22 @@ socket.on("array", (data) => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (productTitle.value && productPrice.value) {
+  if (productTitle.value &&  productDesc.value && productCode.value && productStock.value && productCat.value && productPrice.value) {
     const newProduct = {
       title: productTitle.value,
+      description: productDesc.value,
+      img: "",
       price: productPrice.value,
+      code: productCode.value,
+      stock: productStock.value, 
+      category: productCat.value, 
+      status: "True"
     };
     productTitle.value = "";
+    productDesc.value = "";
+    productCode.value = "";
+    productStock.value = "";
+    productCat.value = "";
     productPrice.value = "";
     socket.emit("product", newProduct);
   } else {
